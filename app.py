94dbe00b839ae9b0a234515b1c3935d7bf678e7f
satyadev307 <mallelasatyadev@gmail.com>
2026-05-25 13:31:14 +0530

    Prepared project for deployment

--- app.py
+++ app.py
@@ -340,4 +340,5 @@ def delete_role(role_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()   
-    app.run(debug=True)
+    if __name__ == "__main__":
+       app.run(host="0.0.0.0", port=5000)
